3b02134bb53380096f73e510dd1add03af914cea
Bryce Hanscomb <bryce@brycehanscomb.com>
2020-11-06 21:45:09 +1100

    vonvert ui to Vue.js

--- src/ui/public/script.js
+++ src/ui/public/script.js
@@ -1,91 +1,116 @@
-"use strict";
+/// <reference path="../../../node_modules/vue/types/umd.d.ts" />
 
 const $ = (s) => document.querySelector(s);
-
 const domain = window.location.hostname;
 const audio = $(".xf-player");
 
-const setTrack = (mediaObject) => {
-  $(".xf-player").setAttribute(
-    "src",
-    `http://${domain}:3001${mediaObject.file}`
-  );
-  $(".xf-current-track-name").innerText = getTrackName(mediaObject);
-};
+new Vue({
+  el: ".xf-root",
+  data: {
+    media: [],
+    currentTrack: null,
+  },
+  mounted() {
+    fetch(`http://${domain}:3001/media/all`)
+      .then((res) => res.json())
+      .then((files) => {
+        const firstFile = files[0];
+        this.setTrack(firstFile);
+        this.setQueue(files);
+      });
+  },
+  methods: {
+    handlePlayPauseButtonPressed(e) {
+      const { audio } = this.$refs;
+      if (audio.paused) {
+        audio.play();
+      } else {
+        audio.pause();
+      }
+    },
+    setTrack(mediaObject) {
+      this.currentTrack = mediaObject;
+    },
+    handleTrackButtonPressed(track) {
+      // audio crashes if not paused before change
+      this.$refs.audio.pause();
+      this.currentTrack = track;
 
-const doSearch = async (q) => {
-  const results = await fetch(
-    `http://${domain}:3001/media/search?q=${q}`
-  ).then((res) => res.json());
+      // have to wait till the src change propagates to the dom
+      Vue.nextTick(() => {
+        this.$refs.audio.play();
+      });
+    },
+    getTrackName(file) {
+      if (!file) {
+        return "Unknown";
+      }
 
-  setQueue(results);
-};
+      const { title, artist } = file.meta.common;
+      const filename = file.file;
 
-const getTrackName = (file) => {
-  const { title, artist } = file.meta.common;
-  const filename = file.file;
+      if (title && artist) {
+        return `${title} - ${artist}`;
+      }
 
-  if (title && artist) {
-    return `${title} - ${artist}`;
-  }
+      if (title || artist) {
+        return title || artist;
+      }
 
-  if (title || artist) {
-    return title || artist;
-  }
+      return filename.split("/").reverse()[0];
+    },
+    setQueue(files) {
+      this.media = files;
+    },
+    async doSearch(q) {
+      const results = await fetch(
+        `http://${domain}:3001/media/search?q=${q}`
+      ).then((res) => res.json());
 
-  return filename.split("/").reverse()[0];
-};
+      this.setQueue(results);
+    },
+    handSearchQueryChanged(e) {
+      this.doSearch(e.target.value);
+    },
+  },
+  computed: {
+    playerState() {
+      if (!this.$refs.audio) {
+        return "beh";
+      }
 
-const setQueue = (files) => {
-  /**
-   * @type {HTMLElement}
-   */
-  const select = $(".xf-track-select");
+      return this.$refs.audio.paused ? "paused" : "playing";
+    },
+    trackSrc() {
+      if (!this.currentTrack) {
+        return "";
+      }
 
-  [...select.children].forEach((n) => n.remove());
-
-  files.forEach((file) => {
-    const o = document.createElement("button");
-    o.classList.add("xf-track-select-button");
-    o.setAttribute("data-value", file.file);
-    o.innerText = getTrackName(file);
-    o.onclick = () => {
-      audio.pause();
-      setTrack(file);
-      audio.play();
-    };
-    console.log("adding", file);
-    select.appendChild(o);
-  });
-};
-
-fetch(`http://${domain}:3001/media/all`)
-  .then((res) => res.json())
-  .then((files) => {
-    const firstFile = files[0];
-    setTrack(firstFile);
-    setQueue(files);
-  });
-
-$(".xf-search-box").addEventListener("input", (e) => doSearch(e.target.value));
-
-audio.addEventListener("pause", (e) => {
-  $(".xf-play-or-pause").dataset.state = "paused";
+      return `http://${domain}:3001${this.currentTrack.file}`;
+    },
+    trackName() {
+      return this.getTrackName(this.currentTrack);
+    },
+  },
 });
 
-audio.addEventListener("play", (e) => {
-  $(".xf-play-or-pause").dataset.state = "playing";
-});
-
-$(".xf-play-or-pause").addEventListener("click", (e) => {
-  /**
-   * @type {HTMLAudioElement}
-   */
-  const { dataset } = e.target;
-
-  if (dataset.state === "playing") {
-    audio.pause();
-  } else {
-    audio.play();
-  }
-});
+// audio.addEventListener("pause", (e) => {
+//   $(".xf-play-or-pause").dataset.state = "paused";
+// });
+//
+// audio.addEventListener("play", (e) => {
+//   $(".xf-play-or-pause").dataset.state = "playing";
+// });
+//
+// $(".xf-play-or-pause").addEventListener("click", (e) => {
+//   /**
+//    * @type {HTMLAudioElement}
+//    */
+//   const { dataset } = e.target;
+//
+//   if (dataset.state === "playing") {
+//     audio.pause();
+//   } else {
+//     audio.play();
+//   }
+// });
